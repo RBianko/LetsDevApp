0051388b67877e2d09e0e0a3932e035f295a1254
roman-bianko <skayone1@gmail.com>
2021-10-07 18:04:21 +0300

    main page layout

---
+++ src/components/main-page/logo/logo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './logo.css'
+
+const Logo = () => {
+    return (
+        <a className="logo" href="#main">
+            <span className="logo-L">L</span>
+            <span className="logo-D">D</span>
+        </a>
+    )
+}
+
+export default Logo
