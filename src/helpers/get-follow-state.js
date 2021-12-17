751b979b765be12b6ceb5c7552ab2b8c8d7a76ff
roman-bianko <skayone1@gmail.com>
2021-12-17 14:16:07 +0300

    getFollowState helper func refactor

--- src/helpers/get-follow-state.js
+++ src/helpers/get-follow-state.js
@@ -1,13 +1,11 @@
 const getFollowState = (currentUser, user) => {
-    let followState = ''
     const isFollowing = currentUser.follow.following.some(id => id === user._id)
     const isFollower = currentUser.follow.followers.some(id => id === user._id)
 
     const isFollow = isFollowing ? false : 'Follow'
     const isFollowBack = isFollower && !isFollowing ? 'Follow Back' : false
-    followState = isFollowBack || isFollow || 'Unfollow'
 
-    return followState
+    return isFollowBack || isFollow || 'Unfollow'
 }
 
 export default getFollowState
